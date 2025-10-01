7c3c8c4bc3379c112a2d8ddafefdaab002930cd8
mmella-code <150011614+mmella-code@users.noreply.github.com>
2025-10-01 22:50:27 +0300

    Update coffee.js

--- coffee.js
+++ coffee.js
@@ -1,1 +1,18 @@
 const searchInput = document.getElementById('searchInput');
+const coffeetypes = document.querySelectorAll('.coffee-type');
+
+searchInput.addEventListener('keyup', function () { const searchText = this.value.toLowerCase();
+coffeetypes.forEach(card => { const coffeeName = card.querySelector('h3').textContent.toLowerCase();
+const coffeeDesc = card.querySelector('p').textContent.toLowerCase();
+if (coffeeName.includes (searchText) || cofeeDesc.includes(searchText)
+    { 
+  card.classicList.remove('hidden'); 
+} else {
+  card.classList.add('hidden'); }
+                            });
+                                                  });
+                                                   
+
+                            
+                            
+                              
